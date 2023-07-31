da85d2ef1fe60f511f7a307832f1847a889ebab9
Chigala <92148630+Chigala@users.noreply.github.com>
2023-07-31 07:56:57 +0100

    remove redundant functions

--- src/dashboard.js
+++ src/dashboard.js
@@ -52,150 +52,150 @@ const Dashboard = () => {
     return response.json();
   };
 
-  const createPsbt = async (
-    paymentPublicKeyString,
-    ordinalsPublicKeyString,
-    paymentUnspentOutputs,
-    ordinalsUnspentOutputs,
-    recipient1,
-    recipient2
-  ) => {
-    const bitcoinTestnet = {
-      bech32: "tb",
-      pubKeyHash: 0x6f,
-      scriptHash: 0xc4,
-      wif: 0xef,
-    };
+  // const createPsbt = async (
+  //   paymentPublicKeyString,
+  //   ordinalsPublicKeyString,
+  //   paymentUnspentOutputs,
+  //   ordinalsUnspentOutputs,
+  //   recipient1,
+  //   recipient2
+  // ) => {
+  //   const bitcoinTestnet = {
+  //     bech32: "tb",
+  //     pubKeyHash: 0x6f,
+  //     scriptHash: 0xc4,
+  //     wif: 0xef,
+  //   };
 
-    // choose first unspent output
-    const paymentOutput = paymentUnspentOutputs[0];
-    const ordinalOutput = ordinalsUnspentOutputs[0];
+  //   // choose first unspent output
+  //   const paymentOutput = paymentUnspentOutputs[0];
+  //   const ordinalOutput = ordinalsUnspentOutputs[0];
 
-    const paymentPublicKey = hex.decode(paymentPublicKeyString);
-    const ordinalPublicKey = hex.decode(ordinalsPublicKeyString);
+  //   const paymentPublicKey = hex.decode(paymentPublicKeyString);
+  //   const ordinalPublicKey = hex.decode(ordinalsPublicKeyString);
 
-    const tx = new btc.Transaction();
+  //   const tx = new btc.Transaction();
 
-    // create segwit spend
-    const p2wpkh = btc.p2wpkh(paymentPublicKey, bitcoinTestnet);
-    const p2sh = btc.p2sh(p2wpkh, bitcoinTestnet);
+  //   // create segwit spend
+  //   const p2wpkh = btc.p2wpkh(paymentPublicKey, bitcoinTestnet);
+  //   const p2sh = btc.p2sh(p2wpkh, bitcoinTestnet);
 
-    // create taproot spend
-    const p2tr = btc.p2tr(ordinalPublicKey, undefined, bitcoinTestnet);
+  //   // create taproot spend
+  //   const p2tr = btc.p2tr(ordinalPublicKey, undefined, bitcoinTestnet);
 
-    // set transfer amount and calculate change
-    const fee = 300n; // set the miner fee amount
-    const recipient1Amount = bigInt(Math.min(paymentOutput.value, 3000)) - fee;
-    const recipient2Amount = bigInt(Math.min(ordinalOutput.value, 3000));
-    const total = recipient1Amount + recipient2Amount;
-    const changeAmount =
-      bigInt(paymentOutput.value) + bigInt(ordinalOutput.value) - total - fee;
+  //   // set transfer amount and calculate change
+  //   const fee = 300n; // set the miner fee amount
+  //   const recipient1Amount = bigInt(Math.min(paymentOutput.value, 3000)) - fee;
+  //   const recipient2Amount = bigInt(Math.min(ordinalOutput.value, 3000));
+  //   const total = recipient1Amount + recipient2Amount;
+  //   const changeAmount =
+  //     bigInt(paymentOutput.value) + bigInt(ordinalOutput.value) - total - fee;
 
-    // payment input
-    tx.addInput({
-      txid: paymentOutput.txid,
-      index: paymentOutput.vout,
-      witnessUtxo: {
-        script: p2sh.script ? p2sh.script : Buffer.alloc(0),
-        amount: bigInt(paymentOutput.value),
-      },
-      redeemScript: p2sh.redeemScript ? p2sh.redeemScript : Buffer.alloc(0),
-      witnessScript: p2sh.witnessScript,
-      sighashType: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
-    });
+  //   // payment input
+  //   tx.addInput({
+  //     txid: paymentOutput.txid,
+  //     index: paymentOutput.vout,
+  //     witnessUtxo: {
+  //       script: p2sh.script ? p2sh.script : Buffer.alloc(0),
+  //       amount: bigInt(paymentOutput.value),
+  //     },
+  //     redeemScript: p2sh.redeemScript ? p2sh.redeemScript : Buffer.alloc(0),
+  //     witnessScript: p2sh.witnessScript,
+  //     sighashType: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
+  //   });
 
-    // ordinals input
-    tx.addInput({
-      txid: ordinalOutput.txid,
-      index: ordinalOutput.vout,
-      witnessUtxo: {
-        script: p2tr.script,
-        amount: bigInt(ordinalOutput.value),
-      },
-      tapInternalKey: ordinalPublicKey,
-      sighashType: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
-    });
+  //   // ordinals input
+  //   tx.addInput({
+  //     txid: ordinalOutput.txid,
+  //     index: ordinalOutput.vout,
+  //     witnessUtxo: {
+  //       script: p2tr.script,
+  //       amount: bigInt(ordinalOutput.value),
+  //     },
+  //     tapInternalKey: ordinalPublicKey,
+  //     sighashType: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
+  //   });
 
-    tx.addOutputAddress(recipient1, recipient1Amount, bitcoinTestnet);
-    tx.addOutputAddress(recipient2, recipient2Amount, bitcoinTestnet);
-    tx.addOutputAddress(recipient2, changeAmount, bitcoinTestnet);
+  //   tx.addOutputAddress(recipient1, recipient1Amount, bitcoinTestnet);
+  //   tx.addOutputAddress(recipient2, recipient2Amount, bitcoinTestnet);
+  //   tx.addOutputAddress(recipient2, changeAmount, bitcoinTestnet);
 
-    const psbt = tx.toPSBT(0);
-    const psbtB64 = base64.encode(psbt);
-    return psbtB64;
-  };
+  //   const psbt = tx.toPSBT(0);
+  //   const psbtB64 = base64.encode(psbt);
+  //   return psbtB64;
+  // };
 
-  const onSignTransactionClick = async () => {
-    const paymentUnspentOutputs = await getUnspent(paymentAddress);
-    console.log("this is the unspent outputs", paymentUnspentOutputs);
-    const ordinalsUnspentOutputs = await getUnspent(ordinalsAddress);
+  // const onSignTransactionClick = async () => {
+  //   const paymentUnspentOutputs = await getUnspent(paymentAddress);
+  //   console.log("this is the unspent outputs", paymentUnspentOutputs);
+  //   const ordinalsUnspentOutputs = await getUnspent(ordinalsAddress);
 
-    if (paymentUnspentOutputs.length < 1) {
-      alert("No unspent outputs found for payment address");
-    }
+  //   if (paymentUnspentOutputs.length < 1) {
+  //     alert("No unspent outputs found for payment address");
+  //   }
 
-    if (ordinalsUnspentOutputs.length < 1) {
-      alert("No unspent outputs found for ordinals address");
-    }
+  //   if (ordinalsUnspentOutputs.length < 1) {
+  //     alert("No unspent outputs found for ordinals address");
+  //   }
 
-    // create psbt sending from payment address to ordinals address
-    const outputRecipient1 = ordinalsAddress;
-    const outputRecipient2 = paymentAddress;
+  //   // create psbt sending from payment address to ordinals address
+  //   const outputRecipient1 = ordinalsAddress;
+  //   const outputRecipient2 = paymentAddress;
 
-    const psbtBase64 = await createPsbt(
-      paymentPublicKey,
-      ordinalsPublicKey,
-      paymentUnspentOutputs,
-      ordinalsUnspentOutputs,
-      outputRecipient1,
-      outputRecipient2
-    );
+  //   const psbtBase64 = await createPsbt(
+  //     paymentPublicKey,
+  //     ordinalsPublicKey,
+  //     paymentUnspentOutputs,
+  //     ordinalsUnspentOutputs,
+  //     outputRecipient1,
+  //     outputRecipient2
+  //   );
 
-    const signPsbtOptions = {
-      payload: {
-        network: {
-          type: "Mainnet",
-        },
-        message: "Sign Transaction",
-        psbtBase64: psbtBase64,
-        broadcast: false,
-        inputsToSign: [
-          {
-            address: paymentAddress,
-            signingIndexes: [0],
-            sigHash: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
-          },
-          {
-            address: ordinalsAddress,
-            signingIndexes: [1],
-            sigHash: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
-          },
-        ],
-      },
-      onFinish: (response) => {
-        alert(response.psbtBase64);
-      },
-      onCancel: () => alert("Canceled"),
-    };
-    await signTransaction(signPsbtOptions);
-  };
+  //   const signPsbtOptions = {
+  //     payload: {
+  //       network: {
+  //         type: "Mainnet",
+  //       },
+  //       message: "Sign Transaction",
+  //       psbtBase64: psbtBase64,
+  //       broadcast: false,
+  //       inputsToSign: [
+  //         {
+  //           address: paymentAddress,
+  //           signingIndexes: [0],
+  //           sigHash: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
+  //         },
+  //         {
+  //           address: ordinalsAddress,
+  //           signingIndexes: [1],
+  //           sigHash: btc.SignatureHash.SINGLE | btc.SignatureHash.ANYONECANPAY,
+  //         },
+  //       ],
+  //     },
+  //     onFinish: (response) => {
+  //       alert(response.psbtBase64);
+  //     },
+  //     onCancel: () => alert("Canceled"),
+  //   };
+  //   await signTransaction(signPsbtOptions);
+  // };
 
-  const onSignMessageClick = async () => {
-    const signMessageOptions = {
-      payload: {
-        network: {
-          type: "Mainnet",
-        },
-        address: ordinalsAddress,
-        message: "Sign Transaction",
-      },
-      onFinish: (response) => {
-        alert(response);
-      },
-      onCancel: () => alert("Canceled"),
-    };
-    await signMessage(signMessageOptions);
-  };
+  // const onSignMessageClick = async () => {
+  //   const signMessageOptions = {
+  //     payload: {
+  //       network: {
+  //         type: "Mainnet",
+  //       },
+  //       address: ordinalsAddress,
+  //       message: "Sign Transaction",
+  //     },
+  //     onFinish: (response) => {
+  //       alert(response);
+  //     },
+  //     onCancel: () => alert("Canceled"),
+  //   };
+  //   await signMessage(signMessageOptions);
+  // };
 
   const onSendBtcClick = async () => {
     const sendBtcOptions = {
